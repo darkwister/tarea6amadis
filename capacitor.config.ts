4b467f3f436966b45022dc8a2381efd67aac2048
DarkWister <darkwistercrak17@gmail.com>
2025-03-03 23:37:30 +0000

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.cotueaoapp',
+  appName: 'cotueaoapp',
+  webDir: 'www'
+};
+
+export default config;
