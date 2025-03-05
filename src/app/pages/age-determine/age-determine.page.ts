07cef2a379fc0309ab803392f764690c442db867
DarkWister <darkwistercrak17@gmail.com>
2025-03-05 01:07:54 +0000

    age module

--- src/app/pages/age-determine/age-determine.page.ts
+++ src/app/pages/age-determine/age-determine.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonLabel } from '@ionic/angular/standalone';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-age-determine',
   templateUrl: './age-determine.page.html',
   styleUrls: ['./age-determine.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonButton, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HttpClientModule]
 })
 export class AgeDeterminePage implements OnInit {
+  targetName?: string;
+  age?: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  getData(name: string): any {
+    const url = `https://api.agify.io/?name=${name}`;
+    this.http.get(url).subscribe((response: any) =>{
+      this.age = response.age;
+      console.log(response.age);
+      console.log(response);
+    }
+    , error => {
+      console.log(error);
+    })
+  }
 }
