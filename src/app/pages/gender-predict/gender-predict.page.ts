fe0d6e77ed655e7a1b3e2c4e11707fa7e1d28004
DarkWister <darkwistercrak17@gmail.com>
2025-03-05 00:43:46 +0000

    gender module done, missing css and :sparkles:

--- src/app/pages/gender-predict/gender-predict.page.ts
+++ src/app/pages/gender-predict/gender-predict.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-gender-predict',
   templateUrl: './gender-predict.page.html',
   styleUrls: ['./gender-predict.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput, IonLabel, IonButton, HttpClientModule]
 })
 export class GenderPredictPage implements OnInit {
-
-  constructor() { }
+  targetName?: string;
+  gender?: string;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  getData(name: string): any {
+    const url = `https://api.genderize.io/?name=${name}`;
+    this.http.get(url).subscribe((response: any) => {
+      this.gender = response.gender
+      console.log(this.gender);
+      console.log(response.probability);
+    }, error => {
+      console.log(error);
+    })
+  }
+  
 }
