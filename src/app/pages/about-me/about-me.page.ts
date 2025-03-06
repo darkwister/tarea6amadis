0a44ee631f4aa5c3319b2d042231b871816fc3d8
DarkWister <darkwistercrak17@gmail.com>
2025-03-06 22:19:51 +0000

    about me module finished

--- src/app/pages/about-me/about-me.page.ts
+++ src/app/pages/about-me/about-me.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonLabel, IonRow, IonItem, IonCardContent, IonCardSubtitle, IonCardHeader, IonCardTitle, IonCard, IonImg, IonAvatar, IonList } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoGithub, logoLinkedin } from 'ionicons/icons';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.page.html',
   styleUrls: ['./about-me.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonList, IonAvatar, IonImg, IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonCardContent, IonItem, IonRow, IonLabel, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AboutMePage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({logoGithub,logoLinkedin});
+  }
 
   ngOnInit() {
   }
