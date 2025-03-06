8a6ef135fca62d0c9eb71408fef4363cf6282e7c
DarkWister <darkwistercrak17@gmail.com>
2025-03-06 21:57:02 +0000

    removed debug on weather report

--- src/app/pages/weather-repo-rd/weather-repo-rd.page.ts
+++ src/app/pages/weather-repo-rd/weather-repo-rd.page.ts
@@ -25,7 +25,6 @@ export class WeatherRepoRdPage implements OnInit {
     this.weatherService.getWeatherData(latitude, longitude).subscribe(
       (data) => {
         this.weatherData = data;
-        console.log('Clima:', this.weatherData);
         this.isLoading = false;
       },
       (error) => {
